9bbd00b0fd7a8273d1556d5c953c006c4fd7f377
erun94510 <erun9414@icloud.com>
2023-03-17 00:33:42 +0900

    [Feat] 메모 추가하는 기능 구현

--- memo/memo/CreateMemoView.swift
+++ memo/memo/CreateMemoView.swift
@@ -8,11 +8,32 @@
 import Foundation
 import UIKit
 
-class CreateMemoView: UIViewController {
+class CreateMemoView: UIViewController, UITextFieldDelegate {
+
+    lazy var textView: UITextView = {
+        let width: CGFloat = self.view.bounds.width
+        let height: CGFloat = self.view.bounds.height
+        let posX: CGFloat = (self.view.bounds.width - width) / 2
+        let posY: CGFloat = (self.view.bounds.height - height) / 2
+        let textView = UITextView(frame: CGRect(x: posX, y: posY, width: width, height: height))
+        textView.font = UIFont.systemFont(ofSize: 15)
+        return textView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(self.textView)
         self.view.backgroundColor = .white
+        
+        let createButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createMemoButton(_:)))
+        self.navigationItem.rightBarButtonItem = createButton
     }
+    
+    @objc func createMemoButton(_ sender: Any) {
+        let content = self.textView.text
+        let _ = ViewController().createMemoData(content: content!, createdOrEditAtDate: Date.now)
+    }
+    
 }
+
